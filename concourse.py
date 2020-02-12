c362ee2c8be47129609aa4b43e3dd593322b2dc9
Ulrich Kramer <u.kramer@sap.com>
2020-02-12 20:43:19 +0100

    -

--- concourse.py
+++ concourse.py
@@ -4,55 +4,137 @@
 import yaml
 import sys
 import os
+import json
+import shutil
 
-class Task(yaml.YAMLObject):
-    yaml_tag = u'!Task'
-    def __init__(self, name, timeout, image_resource,script):
-        self.task = name
-        self.timeout = timeout
-        self.config =  {
-          "platform" : "linux",
-          "image_resource": image_resource,
-          "run" : {
-            "path": "/usr/bin/python3",
-            "args" : script,
-          }
+CACHE_DIR = 'tasks'
+class Task:
+  def __init__(self, name, jobname, timeout, image_resource,script,inputs):
+      self.name = name
+      self.timeout = timeout
+      self.config =  {
+        "platform" : "linux",
+        "image_resource": image_resource,
+        "outputs": [ { "name" : CACHE_DIR} ],
+        "inputs": list(map(lambda x: {"name": x}, inputs)) ,
+        "run" : {
+          "path": "/usr/bin/python3",
+          "args" : [ script, jobname , name ],
         }
+      }
+  def concourse(self):
+    return {
+      "task" : self.name,
+      "timeout" : self.timeout, 
+      "config" : self.config,
+    }
 
-def noop(self, *args, **kw):
-    pass
+class GitRepo:
+  def __init__(self, uri):
+      self.uri = uri
+  def concourse(self,name):
+    return {
+      "name": name,
+      "type": "git",
+      "source": {
+          "uri" : self.uri,
+          "branch": "master",
+      }
+    }
+  def dir(self,name):
+    return "~/workspace/" + name
 
+
+class GetTask:
+  def __init__(self, name,trigger,resource):
+    self.name = name
+    self.trigger = trigger
+    self.resource = resource
+  def concourse(self):
+    return {
+      "get": self.name,
+      "trigger": self.trigger,
+    }
+
+
+class Job:
+  def __init__(self, name, script,image_resource,resources):
+    self.name = name
+    self.plan =  []
+    self.last_task = None
+    self.image_resource = image_resource
+    self.script = script
+    self.resources = resources
+    self.inputs = []
+
+  def get(self,name,trigger=False):
+    resource = self.resources[name]
+    self.plan.append(GetTask(name,trigger,resource))
+    self.inputs.append(name)
+    return resource.dir(name)
+
+  def task(self,name,fun,timeout="5m",image_resource=None,resources=[]):
+    if  not image_resource:
+      image_resource = self.image_resource
+    inputs = self.inputs
+    if self.last_task:
+      inputs = inputs + [CACHE_DIR]
+    self.plan.append(Task(name,self.name,timeout,image_resource,self.script,inputs))
+    cache_file = os.path.join(CACHE_DIR, self.name, name + ".json")
+    def fn():
+      print("Running: {}".format(name))
+      os.makedirs(os.path.dirname(cache_file), exist_ok=True)
+      try:
+        with open(cache_file,'r') as fd:
+          return json.load(fd)
+      except FileNotFoundError:
+        result = fun()
+        with open(cache_file,'w') as fd:
+          json.dump(result,fd)
+        return result
+    self.last_task = fn
+    return self.last_task
+
+  def concourse(self):
+    return {
+      "name" : self.name,
+      "plan" : list(map(lambda x: x.concourse(), self.plan))
+    }
+  
+  def run(self):
+    return self.last_task()
 
 
 
 class Pipeline():
   def __init__(self,name,script):
-    self.steps = []
+    self.jobs = []
+    self.resources = {}
     self.name = name
-    self.last_task = None
     self.image_resource = "ci-image"
     self.script = script
 
-
-  def task(self,name,fun,timeout="5m",image_resource=None,resources=[]):
-    if  not image_resource:
-      image_resource = self.image_resource
-    self.steps.append(Task(name,timeout,image_resource,self.script))
-    def fn():
-      print("Running: {}".format(name))
-      return fun()
-    self.last_task = fn
-    return self.last_task
-
   def run(self):
-    return self.last_task()
+    shutil.rmtree(CACHE_DIR)
+    for job in self.jobs:
+      job.run()
+  
+  def job(self,name):
+    result = Job(name,self.script,self.image_resource,self.resources)
+    self.jobs.append(result)
+    return result
+
+  def resource(self,name,resource):
+    self.resources[name] = resource
 
   def concourse(self):
-    yaml.emitter.Emitter.process_tag = noop
-    yaml.dump({"plan": self.steps}, sys.stdout, allow_unicode=True)
+    return {
+      "resources" : list(map(lambda kv: kv[1].concourse(kv[0]), self.resources.items())),
+      "jobs" : list(map(lambda x: x.concourse(), self.jobs)),
+    }
 
 
-def create_cluster(cluster_name):
+def create_shoot(cluster_name):
   print("Create cluster {}".format(cluster_name))
   return cluster_name
 
@@ -61,13 +143,16 @@ def install_shalm(shalm_dir,create_cluster):
   print("Installing shalm {} into {}".format(shalm_dir,cluster_name))
   return "Hello"
 
-def pipeline(shalm_dir):
-  p = Pipeline("create-cluster",os.path.basename(__file__))
-  task_create_cluster = p.task("create-cluster",lambda : create_cluster("xxx"))
-  p.task("install-shalm",lambda : install_shalm(shalm_dir,task_create_cluster))
+def pipeline():
+  p = Pipeline("c21s",os.path.basename(__file__))
+  p.resource("c21s",GitRepo("https://github.tools.sap/cki/c21s"))
+  job = p.job("create-cluster")
+  c21s_dir = job.get("c21s")
+  task_create_cluster = job.task("create-shoot",lambda : create_shoot("xxx"))
+  job.task("install-shalm",lambda : install_shalm(c21s_dir,task_create_cluster))
   return p
 
-p = pipeline("shalm_dir")
+p = pipeline()
 
 print(p.run())
-p.concourse()
+yaml.dump(p.concourse(), sys.stdout, allow_unicode=True)
