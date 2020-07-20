222ec07453646498314e6eadc2c054c000c88ad9
Benjamin Haegenlaeuer <benjamin.haegenlaeuer@sap.com>
2020-07-20 13:00:11 +0200

    Add basic gcs support
    Co-authored-by: Johannes Dillmann <j.dillmann@sap.com>

--- concourse.py
+++ concourse.py
@@ -148,6 +148,36 @@ class InitTask:
             }
         }
 
+class GoogleCloudStorageResource:
+    def __init__(self, bucket, regexp, credentials):
+        self.bucket = bucket
+        self.regexp = regexp
+        self.credentials = credentials
+    
+    def resource_type(self):
+        return {
+            "name": "gcs",
+            "type": "docker-image",
+            "source": {
+                "repository": "frodenas/gcs-resource",
+                "tag": "latest",
+            }
+        }
+    def get(self, name):
+        return self
+    
+    def concourse(self, name):
+        result = {
+            "name": name,
+            "type": "gcs",
+            "icon": "file-cloud",
+            "source": {
+                "bucket": self.bucket,
+                "regexp": self.regexp,
+                "json_key": self.credentials,
+            }
+        }
+        return result
 
 class GitRepoResource:
     def __init__(self, name):
