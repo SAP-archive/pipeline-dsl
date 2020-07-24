c4aa686a9311fcac256492e032b228d12b1ccee3
Ulrich Kramer <u.kramer@sap.com>
2020-07-24 07:25:26 +0200

    Add concourse_context to exports

--- pypeline/__init__.py
+++ pypeline/__init__.py
@@ -1,2 +1,2 @@
-from pypeline.concourse import ParallelTask, Task, InitTask, GetTask, PutTask, TryTask, OptionalSecret, Job, Pipeline, Password, shell
+from pypeline.concourse import ParallelTask, Task, InitTask, GetTask, PutTask, TryTask, OptionalSecret, Job, Pipeline, Password, shell, concourse_context
 from pypeline.resources import ConcourseLockResource, GitRepo, GithubRelease, Cron, DockerImage, GoogleCloudStorageResource
