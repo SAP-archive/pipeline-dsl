ea4d987a203527d4aaa26338426ac61b50d037d4
Luca Schimweg <luca.schimweg@sap.com>
2020-08-04 15:47:51 +0200

    CKI-989 Pipeline: Push to latest branch after successful tests
    Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>

--- concourse/pipeline.py
+++ concourse/pipeline.py
@@ -4,6 +4,9 @@ with Pipeline("pypeline") as pipeline:
     pipeline.resource("pypeline", GitRepo("https://github.tools.sap/cki/pypeline",
         username="istio-serviceuser", password="((GITHUB_TOOLS_SAP_TOKEN))", ignore_paths=["concourse/*"]))
 
+    pipeline.resource("pypeline-latest", GitRepo("https://github.tools.sap/cki/pypeline",
+        username="istio-serviceuser", password="((GITHUB_TOOLS_SAP_TOKEN))", ignore_paths=["concourse/*"], branch="latest"))
+
     with pipeline.job("test") as job:
         job.get("pypeline", trigger=True)
 
@@ -11,3 +14,5 @@ with Pipeline("pypeline") as pipeline:
         def install_and_test():
             shell(["make", "install"], cwd="pypeline")
             shell(["make", "test"], cwd="pypeline")
+
+        job.put("pypeline-latest", params={"repository": "pypeline"})
